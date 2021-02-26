171c6cea522fc95ee7c76763059d809ab5dec07d
ishita30 <ishitasinghal30@gmail.com>
2021-02-27 02:36:18 +0530

    Create Number of digits.cpp
    Added a file.

---
+++ Recursion/Number of digits.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int count(int n){
+    if(n < 10){
+        return 1;
+    }
+    return 1 + count(n / 10);
+}
+int main(){
+    int n;
+    cin >> n;
+  
+    cout << count(n) << endl;
+}
+
