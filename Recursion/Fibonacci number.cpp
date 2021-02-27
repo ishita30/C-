bd1bf83da8d6467dc451f42acdb7cb4dc18f9852
ishita30 <ishitasinghal30@gmail.com>
2021-02-27 06:57:45 +0530

    Create Fibonacci number.cpp
    Update

---
+++ Recursion/Fibonacci number.cpp
@@ -0,0 +1,23 @@
+//To find nth digit of fibonacci series 1,1,2,3,5...
+#include <iostream>
+using namespace std;
+int fib(int n) {
+	if (n == 0) {
+		return 0;
+	}
+
+	if (n == 1) {
+		return 1;
+	}
+
+	int a = fib(n - 1);
+	int b = fib(n - 2);
+	return a+b;
+}
+
+int main() {
+    int n;
+    cin>>n;
+	cout << fib(n) << endl;
+}
+
