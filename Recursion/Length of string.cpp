9a94fe2649feb523b41d3c011c820909939e4a29
ishita30 <ishitasinghal30@gmail.com>
2021-03-10 17:08:27 +0530

    Create Length of string.cpp

---
+++ Recursion/Length of string.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int length(char s[]) {
+	if (s[0] == '\0') {
+		return 0;
+	}
+	int smallStringLength = length(s + 1);
+	return 1 + smallStringLength;
+}
+int main() {
+	char str[100];
+	cin >> str;
+
+	int l = length(str);
+	cout << l << endl;
+}
+
+
