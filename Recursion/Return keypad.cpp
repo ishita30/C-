f031553683cd165a259cefe34121e781804f2b27
ishita30 <ishitasinghal30@gmail.com>
2021-06-03 10:15:55 +0530

    Update Return keypad.cpp

--- Recursion/Return keypad.cpp
+++ Recursion/Return keypad.cpp
@@ -1,74 +1,34 @@
 #include <iostream>
+#include<string>
 using namespace std;
-#include <string>
-string getstring(int n)
-{
-    if(n==2)
-    {
-        return "abc";
-    }
-    if(n==3)
-    {
-        return "def";
-    }
-    if(n==4)
-    {
-        return "ghi";
-    }
-    if(n==5)
-    {
-        return "jkl";
-    }
-    if(n==6)
-    {
-        return "mno";
-    }
-    if(n==7)
-    {
-        return "pqrs";
-    }
-    if(n==8)
-    {
-        return "tuv";
-    }
-    if(n==9)
-    {
-        return "wxyz";
-    }
-    return " ";
-}
 
-int keypad(int num, string output[])
+int keypadhelp(int num,string output[],string options[])
 {
     if(num==0)
     {
-        output[0]=" ";
+        output[0]="";//base case
         return 1;
     }
     int lastdigit=num%10;
-    int smallnumber=num/10;
-    
-    int smalloutputsize= keypad(smallnumber,output);
-    string options=getstring(lastdigit);
-    int k= smalloutputsize;
-    for(int i=0; i < options.length()-1; i++)
+    int smallnum=num/10;
+    string smalloutput[500];
+    int smallcount = keypadhelp(smallnum,smalloutput,options);
+    string op = options[lastdigit];
+    int k=0;
+    for(int i=0; i<op.length(); i++)
     {
-        for(int j=0;j<smalloutputsize;j++)
+        for(int j=0;j<smallcount;j++)
         {
-            output[k]= output[j];
+            output[k] = smalloutput[j] + op[i];
             k++;
         }
     }
-    k=0; // to run through the output
-    for(int i=0;i<options.length();i++)
-    {
-        for(int j=0;j<smalloutputsize;j++)
-        {
-            output[k]=output[k] + options[i];
-            k++;
-        }
-    }
-    return smalloutputsize * options.length();
+    return k;
+}
+int keypad(int num,string output[])
+{
+    string options[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    return keypadhelp(num,output,options);
 }
 int main(){
     int num;
@@ -77,9 +37,8 @@ int main(){
     string output[10000];
     int count = keypad(num, output);
     for(int i = 0; i < count && i < 10000; i++){
-        cout<<output[i] << endl;
+        cout << output[i] << endl;
     }
     return 0;
 }
-
 
