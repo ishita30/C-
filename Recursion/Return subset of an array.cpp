6ef80a1a3432aa9e386a5837dde3c15ea46f04b4
ishita30 <ishitasinghal30@gmail.com>
2021-06-12 23:46:48 +0530

    Create Return subset of an array.cpp

---
+++ Recursion/Return subset of an array.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+
+int subset(int input[], int n, int output[][20])
+{
+     if(n<=0)//n is length of array
+    {
+        output[0][0]= 0;
+        return 1;
+               
+    }
+    int smallsize=subset(input+1,n-1,output);
+    for(int i=0;i<smallsize ;i++)
+    {
+       output[smallsize+i][0]=output[i][0]+1;
+       output[i+smallsize][1]=input[0];
+         for(int j=(output[i][0]+1);j>1;j--)
+    {
+                   
+        output[i+smallsize][j]=output[i][j-1];
+    }
+    }
+   
+    return 2*smallsize;
+}
+int main() {
+  int input[20],length, output[35000][20];
+  cin >> length;
+  for(int i=0; i < length; i++)
+    cin >> input[i];
+  
+  int size = subset(input, length, output);
+
+  for( int i = 0; i < size; i++) { 
+	for( int j = 1; j <= output[i][0]; j++) { 
+		cout << output[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
