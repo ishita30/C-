88fa72bf3dcc7d0ea194f4e6841bbe59fc8ab6ce
ishita30 <ishitasinghal30@gmail.com>
2021-06-24 10:41:19 +0530

    Update Return subset of an array.cpp

--- Recursion/Return subset of an array.cpp
+++ Recursion/Return subset of an array.cpp
@@ -1,7 +1,5 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-
 int subset(int input[], int n, int output[][20])
 {
      if(n<=0)//n is length of array
@@ -13,12 +11,12 @@ int subset(int input[], int n, int output[][20])
     int smallsize=subset(input+1,n-1,output);
     for(int i=0;i<smallsize ;i++)
     {
-       output[smallsize+i][0]=output[i][0]+1;
-       output[i+smallsize][1]=input[0];
+       output[smallsize+i][0]=output[i][0]+1;//incrementing size by 1
+       output[i+smallsize][1]=input[0];//copying first number in coloumn 1
          for(int j=(output[i][0]+1);j>1;j--)
     {
                    
-        output[i+smallsize][j]=output[i][j-1];
+        output[i+smallsize][j]=output[i][j-1]; // for copying rest of the elements after input[0] in coloumn 1.
     }
     }
    
@@ -33,8 +31,9 @@ int main() {
   int size = subset(input, length, output);
 
   for( int i = 0; i < size; i++) { 
-	for( int j = 1; j <= output[i][0]; j++) { 
-		cout << output[i][j] << " ";
+	for( int j = 1; j <= output[i][0]; j++) 
+	{ 
+		cout << output[i][j] << " "; //printing elements row wise.
     }
     cout << endl;
   }
