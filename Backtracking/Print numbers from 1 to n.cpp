18cb88396c26f5ad86ef4e3e693a8041af9b61d2
ishita30 <ishitasinghal30@gmail.com>
2022-05-12 09:08:48 +0530

    Create Print numbers from 1 to n.cpp

---
+++ Backtracking/Print numbers from 1 to n.cpp
@@ -0,0 +1,20 @@
+//Print 1 TO n by backtracking
+#include<iostream>
+using namespace std;
+
+void print(int i,int n)
+{
+    if(i<1)
+    {
+        return;
+    }
+    print(i-1,n);
+    cout<<i<<endl;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    print(n,n);
+}
